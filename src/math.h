1943e45203ff433bb91118f452bcd5c11b494a00
Steffen Mueller <smueller@cpan.org>
2011-01-31 18:08:51 +0100

    log gamma and helpers

--- src/math.h
+++ src/math.h
@@ -1,4 +1,8 @@
 #ifndef __math_h_
 #define __math_h_
 
+double log_gamma(double x);
+
+double polynomial_1eval(double x, double* a, unsigned int N);
+double polynomial_eval(double x, double* a, unsigned int N)
 #endif
